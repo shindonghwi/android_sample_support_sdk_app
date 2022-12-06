bcde4e7a6a696c89676949155b3fe428278876cb
shindonghwi <ehdgnl8940@naver.com>
2022-12-06 11:40:05 +0900

    chore: lib 0.0.92

--- app/src/main/java/mago/apps/medicaldeviceorotsdk/MainActivity.kt
+++ app/src/main/java/mago/apps/medicaldeviceorotsdk/MainActivity.kt
@@ -2,23 +2,13 @@ package mago.apps.medicaldeviceorotsdk
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import mago.apps.orot_medication.OrotMedicationSDK
-import mago.apps.orot_medication.interfaces.IOrotMedicationSDK
-import mago.apps.orot_medication.model.State
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val sdk = OrotMedicationSDK()
-
-        sdk.setListener(object : IOrotMedicationSDK.MedicationStateListener {
-            override fun onState(state: State, msg: String?) {
-
-            }
-        })
-        sdk.connectServer()
+        Orot
 
 
     }
